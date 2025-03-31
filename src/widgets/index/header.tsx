890b07d73dc6c899f4195998f0590a770700aa56
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2025-03-31 19:00:33 +0900

    Fix the external link icon to use fontawesome

--- src/widgets/index/header.tsx
+++ src/widgets/index/header.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
   label: string;
@@ -10,10 +12,11 @@ export function Header({ label, onClick }: Props) {
     <div className="lc-index-header">
       <h1>{label}</h1>
       {onClick && (
-        <div
-          className="ui-button-icon ui-icon ui-icon-extlink"
+        <FontAwesomeIcon
+          icon={faExternalLink}
+          className="lc-index-header-icon"
           onClick={onClick}
-        ></div>
+        />
       )}
     </div>
   );
